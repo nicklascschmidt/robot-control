b190a03f3fc541b599053faed1ad60c7908eea38
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2021-02-26 11:22:57 -0800

    separated App from Layout for org, App changed to class component and will handle data

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
+import { Component } from 'react';
+import LayoutContainer from './components/LayoutContainer/LayoutContainer.jsx';
 import MainControl from './components/MainControl/MainControl.jsx';
 import RobotState from './components/RobotStatus/RobotStatus.jsx';
 import ButtonsDisplay from './components/ButtonsDisplay/ButtonsDisplay.jsx';
 import CommandHistory from './components/CommandHistory/CommandHistory.jsx';
 import AlertMessage from './components/AlertMessage/AlertMessage.jsx';
 
-const Background = styled.div`
-  background-color: lightgrey;
-  min-height: 100vh;
-  min-width: 100vw;
-`;
 
-const LayoutContainer = styled.div`
-  position: fixed;
-  top: 45%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  height: 70vh;
-  min-width: 80vw;
-
-  @media (min-width: 320px) {
-    /* smartphones, iPhone, portrait 480x320 phones */
-  }
-  @media (min-width: 481px) {
-    /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */
-    min-width: 60vw;
-  }
-  @media (min-width: 641px) {
-    /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */
-    min-width: 50vw;
-  }
-  @media (min-width: 961px) {
-    /* tablet, landscape iPad, lo-res laptops ands desktops */
-  }
-  @media (min-width: 1025px) {
-    /* big landscape tablets, laptops, and desktops */
-  }
-  @media (min-width: 1281px) {
-    /* hi-res laptops and desktops */
+    }
   }
 
-  display: flex;
-  flex-flow: column nowrap;
-  gap: 1em;
-
-  .command-history {
-    flex: 1 1 auto;
-  }
-`;
-
-function App() {
-  return (
-    <Background>
+  render() {
+    return (
       <LayoutContainer>
         <AlertMessage />
         <MainControl>
@@ -62,8 +25,8 @@ function App() {
         </MainControl>
         <CommandHistory className='command-history' />
       </LayoutContainer>
-    </Background>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/LayoutContainer/LayoutContainer.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  background-color: lightgrey;
+  min-height: 100vh;
+  min-width: 100vw;
+`;
+
+const ContentContainer = styled.div`
+  position: fixed;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  height: 70vh;
+  min-width: 80vw;
+
+  @media (min-width: 320px) {
+    /* smartphones, iPhone, portrait 480x320 phones */
+  }
+  @media (min-width: 481px) {
+    /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */
+    min-width: 60vw;
+  }
+  @media (min-width: 641px) {
+    /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */
+    min-width: 50vw;
+  }
+  @media (min-width: 961px) {
+    /* tablet, landscape iPad, lo-res laptops ands desktops */
+  }
+  @media (min-width: 1025px) {
+    /* big landscape tablets, laptops, and desktops */
+  }
+  @media (min-width: 1281px) {
+    /* hi-res laptops and desktops */
+  }
+
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 1em;
+
+  .command-history {
+    flex: 1 1 auto;
+  }
+`;
+
+const LayoutContainer = ({ children }) => {
+  return (
+    <Background>
+      <ContentContainer>
+        { children }
+      </ContentContainer>
+    </Background>
+  );
+}
+
+export default LayoutContainer;
