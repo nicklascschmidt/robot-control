cc2a17e48c24964feec059eb109fab3af2f34412
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2021-02-26 11:03:34 -0800

    added in basic layout in App, will have to separate style for cleanliness

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-import RobotState from './components/RobotState.jsx';
+import React from 'react';
+import styled from 'styled-components';
+import MainControl from './components/MainControl/MainControl.jsx';
+import RobotState from './components/RobotStatus/RobotStatus.jsx';
+import ButtonsDisplay from './components/ButtonsDisplay/ButtonsDisplay.jsx';
+import CommandHistory from './components/CommandHistory/CommandHistory.jsx';
+import AlertMessage from './components/AlertMessage/AlertMessage.jsx';
+
+const Background = styled.div`
+  background-color: lightgrey;
+  min-height: 100vh;
+  min-width: 100vw;
+`;
+
+const LayoutContainer = styled.div`
+  position: fixed;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  height: 70vh;
+  min-width: 80vw;
+
+  @media (min-width: 320px) {
+    /* smartphones, iPhone, portrait 480x320 phones */
+  }
+  @media (min-width: 481px) {
+    /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */
+    min-width: 60vw;
+  }
+  @media (min-width: 641px) {
+    /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */
+    min-width: 50vw;
+  }
+  @media (min-width: 961px) {
+    /* tablet, landscape iPad, lo-res laptops ands desktops */
+  }
+  @media (min-width: 1025px) {
+    /* big landscape tablets, laptops, and desktops */
+  }
+  @media (min-width: 1281px) {
+    /* hi-res laptops and desktops */
+  }
+
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 1em;
+
+  .command-history {
+    flex: 1 1 auto;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <RobotState />
-      </header>
-    </div>
+    <Background>
+      <LayoutContainer>
+        <AlertMessage />
+        <MainControl>
+          <RobotState />
+          <ButtonsDisplay />
+        </MainControl>
+        <CommandHistory className='command-history' />
+      </LayoutContainer>
+    </Background>
   );
 }
 
